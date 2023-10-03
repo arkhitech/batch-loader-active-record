443c5a56a119bd6ee24db7e7b1fa5f032c7159da
Hisham Malik <hisham@arkhitech.com>
2023-10-04 03:54:25 +0500

    avoid NoMethodError causing error by replacing delegate with custom implementation with ::NoMethodError

--- lib/batch_loader_active_record/association_proxy.rb
+++ lib/batch_loader_active_record/association_proxy.rb
@@ -6,7 +6,29 @@ module BatchLoaderActiveRecord
       @logger = logger
     end
 
-    delegate :<<, :to_a, :each, :each_index, :empty?, :first, :last, :map, :collect, :count, :size, to: :@records
+    methods = [:<<, :to_a, :each, :each_index, :empty?, :first, :last, :map, :collect, :count, :size]
+    to = :@records
+    location = caller_locations(1, 1).first
+    file, line = location.path, location.lineno    
+    methods.map do |method|
+      definition = /[^\]]=$/.match?(method) ? "arg" : "*args, &block"
+      exception = %(raise DelegationError, "#{self}##{method} delegated to #{to}.#{method}, but #{to} is nil: \#{self.inspect}")
+
+      method_def = [
+        "def #{method}(#{definition})",
+        " _ = #{to}",
+        "  _.#{method}(#{definition})",
+        "rescue ::NoMethodError => e",
+        "  if _.nil? && e.name == :#{method}",
+        "    #{exception}",
+        "  else",
+        "    raise",
+        "  end",
+        "end"
+      ].join ";"
+
+      module_eval(method_def, file, line)
+    end
 
     private
 
